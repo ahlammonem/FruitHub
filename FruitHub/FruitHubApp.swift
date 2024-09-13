48a4e553d7011fe3afe8a68dff748a03d46a83bf
ahlammonem <57093448+ahlammonem@users.noreply.github.com>
2024-09-13 18:05:41 +0300

    Initial Commit

---
+++ FruitHub/FruitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitHubApp.swift
+//  FruitHub
+//
+//  Created by ahlam on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
